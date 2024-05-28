b1f64319ad9ceff9a9e5e3eb7c92864674226d40
Eouls <ckrlcks00@gmail.com>
2024-05-29 05:54:09 +0900

    바텀네비 버그 수정

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -406,12 +406,11 @@ class MainActivity : AppCompatActivity() {
     private fun initBottomNavigation(){
 
         supportFragmentManager.beginTransaction()
-            .replace(R.id.main_bnv, HomeFragment())
+            .replace(R.id.main_frm, HomeFragment())
             .commitAllowingStateLoss()
 
         binding.mainBnv.setOnItemSelectedListener{ item ->
             when (item.itemId) {
-
                 R.id.homeFragment -> { // HomeFragment 로 전환
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.main_frm, HomeFragment())
