73ad136c5f267479760078f7d2a8a054f3f80d9f
Eouls <ckrlcks00@gmail.com>
2024-05-30 00:42:02 +0900

    좋아요 DB 적용안됨

--- app/src/main/java/com/example/musicplayer/locker/LikedAlbumFragment.kt
+++ app/src/main/java/com/example/musicplayer/locker/LikedAlbumFragment.kt
@@ -5,10 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.musicplayer.Album.Album
+import com.example.musicplayer.Album.AlbumDatabase
+import com.example.musicplayer.Song.Song
 import com.example.musicplayer.databinding.FragmentLockerLikedAlbumBinding
 
 class LikedAlbumFragment: Fragment() {
     lateinit var binding: FragmentLockerLikedAlbumBinding
+    lateinit var albumDB: AlbumDatabase
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +22,30 @@ class LikedAlbumFragment: Fragment() {
     ): View? {
         binding = FragmentLockerLikedAlbumBinding.inflate(inflater, container, false)
 
+        albumDB = AlbumDatabase.getInstance(requireContext())!!
+
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initRecyclerview()
+    }
+
+    private fun initRecyclerview(){
+        binding.lockerLikedAlbumRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+        val albumRVAdapter = LikedAlbumRVAdapter()
+//
+//        LikedAlbumRVAdapter.setMyItemClickListener(object : LikedAlbumRVAdapter.DeleteItemClickListener{
+//            override fun onRemoveItem(albumId: Int) {
+//                albumDB.albumDao().updateIsLikeById(false, albumId)
+//            }
+//
+//        })
+
+        binding.lockerLikedAlbumRv.adapter = albumRVAdapter
+
+        albumRVAdapter.addAlbums(albumDB.albumDao().getLikedAlbums(true) as ArrayList<Album>)
     }
 }
